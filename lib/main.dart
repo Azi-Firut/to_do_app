1203179515cbb141c64d545430a2a3988bbbf042
Zifirut <zifirut@gmail.com>
2021-10-31 23:59:33 +0200

    add button

--- lib/main.dart
+++ lib/main.dart
@@ -21,44 +21,60 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key, required this.title}) : super(key: key);
   final String title;
+
+  @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        centerTitle: true,
+        title: Text(widget.title),
       ),
       body: Center(
         child: ListView.builder(
-            //physics: const BouncingScrollPhysics(),  // not beautiful
             itemCount: todoItems.length,
             itemBuilder: (context, int index) {
               var item = todoItems[index];
               return toDoRow(item, index);
             }),
       ),
+      floatingActionButton: SizedBox(
+        width: 380,
+        child: FloatingActionButton.extended(
+          label: const Text(
+            'Add Note',
+            textScaleFactor: 1.3,
+          ),
+          isExtended: true,
+          // onPressed: () {},
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          icon: const Icon(Icons.add),
+          elevation: 3,
+        ),
+        // showModalBottomSheet(
+        //     context: context,
+        //     backgroundColor: Colors.lightBlue,
+        //     elevation: 10,
+        //     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        //     ...
+        // ),
+      ),
     );
   }
 }
-
-// class _HomePageState extends State<HomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: ListView.builder(
-//             //physics: const BouncingScrollPhysics(),  // not beautiful
-//             itemCount: todoItems.length,
-//             itemBuilder: (context, int index) {
-//               var item = todoItems[index];
-//               return toDoRow(item, index);
-//             }),
-//       ),
-//     );
-//   }
-// }
